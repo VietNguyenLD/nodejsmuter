bb6819704a4da46c2d81ffbfa7a194e054239660
VietNguyenLD <59373260+VietNguyenLD@users.noreply.github.com>
2020-01-02 17:44:05 +0700

    xu ly loi

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => res.render('home'));
 app.post('/singup',upload.single('profile'));
+// handler_error
+app.use((err, req, res, next) => {
+    res.send(err.message);
+});
+
 app.listen(3000, () => console.log('Server start')); 
